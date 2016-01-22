1e84f372f1a80f3bce3e2112d9737969385f75c2
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-01-21 22:48:14 -0800

    use tuples

--- datacleanup/src/main/scala/com/virdis/cleanup/GitDiscover.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/GitDiscover.scala
@@ -24,10 +24,10 @@ object GitDiscover {
     val df = sqlContext.read.json("hdfs://52.34.172.205:9000/gitData/Jan15Days.json")
 
     val topPrjs = gitMetrics.joinAcrossEventsByLangRepo(df)(sqlContext)
-
+    
     topPrjs.map {
       r =>
-        Row(r.getAs[String](1), r.getAs[String](0), r.getAs[Long](3))
+        (r.getAs[String](1), r.getAs[String](0), r.getAs[Long](3))
     }.saveToCassandra("git_project","repos", SomeColumns("language","name","eventsTotal"))
 
   }
