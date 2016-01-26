8f1af29cf413c85157b5f30a98a712e45b8144b4
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-01-25 23:02:04 -0800

    add Constants to help with data frame api

--- datacleanup/src/main/scala/com/virdis/cleanup/Constants.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/Constants.scala
@@ -1,5 +1,7 @@
 package com.virdis.cleanup
 
+import org.joda.time.format.DateTimeFormat
+
 /**
   * Created by sandeep on 1/19/16.
   */
@@ -15,8 +17,26 @@ object Constants {
   val PULL_REQUEST_COMMENT_REVIEW = "PullRequestReviewCommentEvent"
 
   ///// COLUMN NAMES /////
-  val REPO_NAME_COLUMN_NAME = "repo.name"
+  val REPO_NAME_COLUMN = "repo.name"
   val NAME_COLUMN = "name"
   val TOTAL_COLUMN = "total"
-  val LANGUAGE_COLUMN = "payload.pull_request.base.repo.language"
+  val PULL_REQ_LANGUAGE_COLUMN = "payload.pull_request.base.repo.language"
+  val CREATED_AT_COLUMN = "created_at"
+  val USER_LOGIN_COLUMN = "actor.login"
+  val COMMIT_COMMENT_COLUMN = "payload.comment.body"
+  val ISSUES_COMMENT_COLUMN = "payload.issue.body"
+
+  val CCE_REPO_NAME = "ccereponame"
+  val CCE_TIME = "ccedate"
+  val CCE_MONTH = "ccemonth"
+  val CCE_CREATED_AT = "ccecreatedat"
+  val CCE_COMMITTER = "ccelogin"
+  val CCE_COMMENT = "ccecomment"
+
+  val I_MONTH = "imonth"
+  val I_TIME = "itime"
+  val I_COMMITTER = "ilogin"
+  val I_COMMENT ="icomment"
+  ///// SCHEMA /////
+  val CREATED_DATE_TIME = DateTimeFormat.forPattern("")
 }
