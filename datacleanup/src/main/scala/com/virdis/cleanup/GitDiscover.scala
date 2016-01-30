676309cdc3f7f50fda3268e5a7f2a404e89b55ad
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-01-30 15:48:49 -0800

    move from bitbucket to git

--- datacleanup/src/main/scala/com/virdis/cleanup/GitDiscover.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/GitDiscover.scala
@@ -1,10 +1,8 @@
 package com.virdis.cleanup
 
-import com.datastax.spark.connector.cql.CassandraConnector
-import org.apache.spark.sql.cassandra.CassandraSQLContext
 import org.apache.spark.sql.{SaveMode, Row, SQLContext}
 import org.apache.spark.{SparkContext, SparkConf}
-import com.datastax.spark.connector._
+import com.typesafe.config.ConfigFactory
 
 /**
   * Created by sandeep on 1/21/16.
@@ -16,7 +14,7 @@ object GitDiscover {
 
     val conf = new SparkConf(true)
                 .set("spark.executor.memory", "14g")
-                 .set("spark.driver.memory", "14g")
+                .set("spark.driver.memory", "14g")
                 .set("sandeep-cassandra-cluster:git/spark.cassandra.input.split.size_in_mb","64")
                 .set("spark.cassandra.connection.host","172.31.2.69")
                 .setAppName("GitDiscover")
@@ -24,15 +22,24 @@ object GitDiscover {
     val sc = new SparkContext(conf)
 
     val sqlContext = new SQLContext(sc)
-    
+
+    val config = ConfigFactory.load()
+
 
     object gitMetrics extends TopProjectQuery with RepoTimeSeries with DataManipulator
 
 
     try {
-      gitMetrics.topProjects(sqlContext)
-     // gitMetrics.repoTimeSeries(sqlContext)
+      if (!config.getBoolean("job.name.topproject.status")) {
+        gitMetrics.topProjects(sqlContext)
+      }
+      if (!config.getBoolean("job.name.repotimeseries.status")) {
+        gitMetrics.repoTimeSeries(sqlContext)
+      }
 
+      if (!config.getBoolean("job.name.userstats")) {
+
+      }
 
     } catch {
       case e: Exception =>
