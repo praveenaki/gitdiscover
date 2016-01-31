eb6430a79f02b3751489556669865cfc4b76eb3f
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-01-30 21:44:48 -0800

    merge job

--- datacleanup/src/main/scala/com/virdis/cleanup/TopProjectQuery.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/TopProjectQuery.scala
@@ -18,19 +18,13 @@ trait TopProjectQuery {
 
 
   def repoEventCount(df: DataFrame)(implicit sQLContext: SQLContext) = {
-    val repoNameAndLang = pullReqsByLangRepo(df)(sQLContext)
+    val repoNameAndLang = repoAndLanguageDF(df)(sQLContext)
 
     val completeDFWithLang = df.join(repoNameAndLang).where(df("repo.name") === repoNameAndLang("name"))
 
     completeDFWithLang.groupBy(REPO_NAME_COLUMN, LANGUAGE_COLUMN).agg( count(EVENT_TYPE).as("eventstotal") )
   }
 
-  def pullReqsByLangRepo(df: DataFrame)(implicit sqlContext: SQLContext): DataFrame = {
-    val pullReqs = getDataByEventType(df, PULL_REQUEST_EVENT)
-
-    pullReqs.filter(pullReqs(PULL_REQ_LANGUAGE_COLUMN).isNotNull).select(pullReqs(REPO_NAME_COLUMN),
-      pullReqs(PULL_REQ_LANGUAGE_COLUMN))
-  }
 
   def topProjectsByLangRepo(df: DataFrame)(implicit sqlContext: SQLContext): DataFrame = {
 
@@ -52,6 +46,63 @@ trait TopProjectQuery {
         StructField("eventstotal", LongType)
       )
     ))
+  }
+
+  def mergeDFResults(df1: DataFrame, df2: DataFrame)(implicit sQLContext: SQLContext): DataFrame = {
+    val merged = df1.join(df2, "name")
+
+    val rez = merged.map {
+      row =>
+        Row(
+          row.getAs[String](1),
+          row.getAs[String](0),
+          row.getAs[String](2),
+          row.getAs[Long](3) + row.getAs[Long](6)
+        )
+    }
+
+    sQLContext.createDataFrame(rez, new StructType(
+      Array(
+        StructField("date", StringType),
+        StructField("name", StringType),
+        StructField("language", StringType),
+        StructField("eventstotal", LongType)
+      )
+    ))
+  }
+
+  def mergeTopProjectsRes(idx1: Int, idx2: Int)(implicit sQLContext: SQLContext): DataFrame = {
+    val res1 = topProjectsByLangRepo(s3FileHandle(idx1))
+    val res2 = topProjectsByLangRepo(s3FileHandle(idx2))
+    mergeDFResults(res1, res2)
+  }
+
+  def popularprojects(implicit sQLContext: SQLContext) = {
+    val res12 = mergeTopProjectsRes(0, 1)
+
+    val res34 = mergeTopProjectsRes(2,3)
+
+    val res1234 = mergeDFResults(res12, res34)
+
+    val res56 = mergeTopProjectsRes(4,5)
+
+    val res78 = mergeTopProjectsRes(6,7)
+
+    val res5678 = mergeDFResults(res56, res78)
+
+    val res910 = mergeTopProjectsRes(8,9)
+
+    val res1112 = mergeTopProjectsRes(10,11)
+
+    val res912 = mergeDFResults(res910, res1112)
+
+    val inter = mergeDFResults(res1234,res5678)
+
+    val rez =  mergeDFResults(inter, res912)
+
+    rez.write.format("org.apache.spark.sql.cassandra").options(
+      Map("table" -> "popularrepos", "keyspace" -> "git")
+    ).mode(SaveMode.Append).save()
   }
 
   /*
@@ -94,5 +145,6 @@ trait TopProjectQuery {
       .options(Map("table" -> "toprepos", "keyspace" -> "git")).mode(SaveMode.Append).save()
 
   }
+
 
 }
