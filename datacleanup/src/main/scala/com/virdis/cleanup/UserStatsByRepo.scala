27ca7d12f807601de9f1a69fc7776248d21bea0c
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-01-30 21:46:15 -0800

    add join to get lang

--- datacleanup/src/main/scala/com/virdis/cleanup/UserStatsByRepo.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/UserStatsByRepo.scala
@@ -1,7 +1,7 @@
 package com.virdis.cleanup
 
 import com.virdis.cleanup.Constants._
-import org.apache.spark.sql.{GroupedData, DataFrame}
+import org.apache.spark.sql.{SQLContext, GroupedData, DataFrame}
 /**
   * Created by sandeep on 1/27/16.
   */
@@ -9,6 +9,8 @@ import org.apache.spark.sql.{GroupedData, DataFrame}
 case class UserStats(eventType: String, counter: Long)
 
 trait UserStatsByRepo {
+
+  self: DataManipulator =>
 
   def userStatsByRepoName(df: DataFrame, reponame: String, login: String): Long = {
     ALL_EVENTS.foldLeft(0L)((b,a) => countByEvent(df, reponame, login, a))
@@ -18,4 +20,13 @@ trait UserStatsByRepo {
     df.filter(df(REPO_NAME_COLUMN) === reponame && df(USER_LOGIN_COLUMN) === login && df(EVENT_TYPE) === eventType).count()
 
   }
+
+  def process(df: DataFrame)(implicit sQLContext: SQLContext) = {
+    val rnameAndLang = repoAndLanguageDF(df)
+    val selectedColsDF = df.select(
+      df(REPO_NAME_COLUMN), df(USER_LOGIN_COLUMN), df(EVENT_TYPE)
+    )
+    rnameAndLang.join(df, NAME_COLUMN)
+  }
+
 }
