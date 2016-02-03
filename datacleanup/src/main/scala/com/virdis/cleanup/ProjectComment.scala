56b4e265c3226847ad1c11a4aab43d6453598403
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-02-03 14:00:23 -0800

    method to  update and save comments dataframe

--- datacleanup/src/main/scala/com/virdis/cleanup/ProjectComment.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/ProjectComment.scala
@@ -1,7 +1,8 @@
 package com.virdis.cleanup
 
 import com.virdis.cleanup.Constants._
-import org.apache.spark.sql.{Row, SQLContext, DataFrame}
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.sql.{SaveMode, Row, SQLContext, DataFrame}
 import org.apache.spark.sql.functions.explode
 
 /**
@@ -14,32 +15,98 @@ trait ProjectComment {
   def extractComments(df: DataFrame)(sQLContext: SQLContext) = {
 
     val commitEvnts = getDataByEventType(df, COMMIT_COMMENT_EVENT)
-    val commitCommentsDF = commitEvnts.select(REPO_NAME_COLUMN,COMMIT_COMMENT_COLUMN).persist()
+    val commitCommentsDF = commitEvnts.select(
+      commitEvnts(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+      commitEvnts(COMMIT_COMMENT_COLUMN).as(PROJECT_COMMENTS_COMMENT_COLUMN)
+    ).persist()
+
+    updateDataframeAndSave(commitCommentsDF)
 
     val pullReqCommentReviewEvent =  getDataByEventType(df, PULL_REQUEST_COMMENT_REVIEW_EVENT)
+
     val pullRCRFirstDF = pullReqCommentReviewEvent
-      .filter(pullReqCommentReviewEvent(PULL_REQ_COMMENT_REVIEW_COMMENT_COLUMN).isNotNull)
-        .select(REPO_NAME_COLUMN, PULL_REQ_COMMENT_REVIEW_COMMENT_COLUMN).persist()
+         .filter(pullReqCommentReviewEvent(PULL_REQ_COMMENT_REVIEW_COMMENT_COLUMN).isNotNull)
+         .select(pullReqCommentReviewEvent(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+          pullReqCommentReviewEvent(PULL_REQ_COMMENT_REVIEW_COMMENT_COLUMN).as(PROJECT_COMMENTS_COMMENT_COLUMN)
+         ).persist()
+
+    updateDataframeAndSave(pullRCRFirstDF)
 
     val pullRERSecondDF = pullReqCommentReviewEvent
-                            .filter(pullReqCommentReviewEvent("payload.pull_request.body").isNotNull)
-                              .select(REPO_NAME_COLUMN, "payload.pull_request.body").persist()
+      .filter(pullReqCommentReviewEvent("payload.pull_request.body").isNotNull)
+      .select(pullReqCommentReviewEvent(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+        pullReqCommentReviewEvent("payload.pull_request.body").as(PROJECT_COMMENTS_COMMENT_COLUMN)
+      ).persist()
+
+    updateDataframeAndSave(pullRERSecondDF)
 
     val issueCommentEvents = getDataByEventType(df, ISSUE_COMMENT_EVENT)
 
-    val issueCommentEvntFirstDF = issueCommentEvents.select(REPO_NAME_COLUMN, ISSUES_COMMENT_COLUMN).persist()
+    val issueCommentEvntFirstDF = issueCommentEvents
+      .select(
+        issueCommentEvents(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+        issueCommentEvents(ISSUES_COMMENT_COLUMN).as(PROJECT_COMMENTS_COMMENT_COLUMN)
+      ).persist()
+
+    updateDataframeAndSave(issueCommentEvntFirstDF)
+
+
     val issueCommentEvntSecondDF = issueCommentEvents.filter(issueCommentEvents("payload.comment.body").isNotNull)
-                                    .select(REPO_NAME_COLUMN, "payload.comment.body").persist()
+                                    .select(
+                                      issueCommentEvents(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+                                      issueCommentEvents("payload.comment.body").as(PROJECT_COMMENTS_COMMENT_COLUMN)
+                                    ).persist()
 
-
+    updateDataframeAndSave(issueCommentEvntSecondDF)
 
     val prEvent = getDataByEventType(df, PULL_REQUEST_EVENT)
-    val pullReqEventsDF = prEvent.select( prEvent(REPO_NAME_COLUMN), prEvent(PULL_REQ_COMMENT_COLUMN ) ).persist()
+    val pullReqEventsDF = prEvent.select(
+      prEvent(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+      prEvent(PULL_REQ_COMMENT_COLUMN ).as(PROJECT_COMMENTS_COMMENT_COLUMN)
+    ).persist()
+
+    updateDataframeAndSave(pullReqEventsDF)
 
     val pushEvents = getDataByEventType(df, PUSH_EVENT)
-    val pushEvtsDF = pushEvents.select( pushEvents(REPO_NAME_COLUMN), pushEvents("payload.commits")).persist()
+    val pushEvtsDF = pushEvents.select(
+      pushEvents(REPO_NAME_COLUMN).as(PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+      pushEvents("payload.commits")
+    ).persist()
 
-    val peComments = explode(pushEvtsDF("commits.message"))
 
+    /*
+        Flatten exploded comments
+     */
+    val allPEComments = pushEvtsDF.explode("commits.message", PROJECT_COMMENTS_COMMENT_COLUMN){s: Seq[String] => s}
+
+    updateDataframeAndSave(allPEComments)
+
+  }
+
+  def updateDataframeAndSave(df: DataFrame)(implicit sQLContext: SQLContext) = {
+    val ir = df.map {
+      val srt =  java.util.UUID.randomUUID().toString
+
+      row =>
+        Row(
+          row.getAs[String](PROJECT_COMMENTS_PROJ_NAME_COLUMN),
+          row.getAs[String](PROJECT_COMMENTS_COMMENT_COLUMN),
+          srt
+        )
+    }
+    sQLContext.createDataFrame(ir, new StructType(
+      Array(
+        StructField(PROJECT_COMMENTS_PROJ_NAME_COLUMN, StringType),
+        StructField(PROJECT_COMMENTS_COMMENT_COLUMN, StringType),
+        StructField("sorter", StringType)
+      )
+    )).write.format("org.apache.spark.sql.cassandra")
+      .options(Map("table" -> "projectcomments", "keyspace" -> "git")).mode(SaveMode.Append).save()
+  }
+
+  def projectComments(implicit sQLContext: SQLContext) = {
+    S3_FILENAMES.zipWithIndex.foreach {
+      case(_, idx) => extractComments(s3FileHandle(idx))
+    }
   }
 }
