b33b23da0ecd2652474d6bf37b9f2267a8027680
Sandeep Virdi <code.monkey.1982@gmail.com>
2016-02-02 23:56:56 -0800

    find edge across projects

--- datacleanup/src/main/scala/com/virdis/cleanup/EdgeAcrossProjects.scala
+++ datacleanup/src/main/scala/com/virdis/cleanup/EdgeAcrossProjects.scala
@@ -1,6 +1,7 @@
 package com.virdis.cleanup
 
-import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.apache.spark.sql.{SaveMode, Row, SQLContext}
 import Constants._
 /**
   * Created by sandeep on 2/1/16.
@@ -12,32 +13,63 @@ trait EdgeAcrossProjects {
   val userActivityRepo = "useractivityrepo"
   val username = "username"
   val projName = "projectname"
+  val eventcommitter = "eventcommitter"
+  val eventype = "eventtype"
+  val lang = "language"
+
   def findEdge(sQLContext: SQLContext) = {
     val repostatsDF = sQLContext.read
                       .format("org.apache.spark.sql.cassandra")
                       .options(Map( "table" -> "repostats", "keyspace" -> "git" ))
-                      .load().repartition(120) // no of cores
+                      .load()
 
     val userActivityDF = sQLContext.read
                           .format("org.apache.spark.sql.cassandra")
                           .options(Map( "table" -> "repostats", "keyspace" -> "git" ))
-                          .load().repartition(120) // no of cores
+                          .load()
 
     val joinedRepo = repostatsDF.select(
                         repostatsDF(projName),
-                        repostatsDF("eventcommitter").as(username),
-                        repostatsDF("eventtype"),
-                        repostatsDF("language")
+                        repostatsDF(eventcommitter).as(username),
+                        repostatsDF(eventype),
+                        repostatsDF(lang)
                       ).join(
                         userActivityDF.select(
                           userActivityDF("projectname").as(userActivityRepo),
-                          userActivityDF(username)
+                          userActivityDF(username),
+                          userActivityDF("count")
                         ), username
                       ).persist()
 
     val filterdJoin = joinedRepo.filter( joinedRepo(projName) !== joinedRepo(userActivityRepo) ).persist()
 
+    val ir = filterdJoin.map {
+      row =>
+        Row(
+          row.getAs[String](username),
+          row.getAs[String](projName),
+          row.getAs[String](eventype),
+          row.getAs[String](lang),
+          row.getAs[String](userActivityRepo),
+          row.getAs[Int]("count"),
+          java.util.UUID.randomUUID().toString
 
+        )
+    }
 
+    val finalResult = sQLContext.createDataFrame(ir, new StructType(
+      Array(
+        StructField(username, StringType),
+        StructField(projName, StringType),
+        StructField(eventype, StringType),
+        StructField(lang, StringType),
+        StructField(userActivityRepo, StringType),
+        StructField("count", IntegerType),
+        StructField("sorter", StringType)
+      )
+    ))
+
+    finalResult.write.format("org.apache.spark.sql.cassandra")
+      .options(Map("table" -> "edgeinfo", "keyspace" -> "git")).mode(SaveMode.Append).save()
   }
 }
